1c8290b0904764fc3eac959c80ea2a9bfc5889f0
Jade Japhet Rugas <rugas.jadejaphet.123@gmail.com>
2024-10-06 22:08:22 +0800

    Working with middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UserController } from './controller/user/user.controller';
 import { UserService } from './services/user/user.service';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
     controllers: [UserController],
     providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        // applyies to all routes
+        // consumer.apply(AuthMiddleware).forRoutes('user');
+        // consumer.apply(AuthMiddleware).forRoutes(UserController);   // using User controller
+
+
+        // Explicit application
+        consumer.apply(AuthMiddleware).forRoutes({
+            path: 'user',                       // specific path
+            method: RequestMethod.GET,          // http method used
+        });
+    }
+}
